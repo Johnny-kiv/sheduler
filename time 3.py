f853a7a443952b17efce5c5ab5170e3f2b257f8f
Johnny <johnny.kiv@yandex.ru>
2021-02-18 13:55:21 +0300

    сс

--- time 3.py
+++ time 3.py
@@ -4,14 +4,25 @@
 import time
 a2 = int(input("Введите минуты работы: "))
 b2 = int(input("Введите минуты отдыха: "))
+с2 = int(input("Введите часы нахождения в школе: "))
 if a2 == 0:
     a4 = 60*20
 if b2 == 0:
     b4 = 20*60
+if с2 == 0:
+    с4 = с2*(60*60)
 else:
     a4 = a2*60
     b4 = b2*60
-    
+    с4 = с2*(60*60)
+print("\a Работаем на компьютере")
+time.sleep(a4)
+print("\a \n \t Идём в школу")
+time.sleep(с4)
+print("\a Работаем на компьютере")
+time.sleep(a4)
+print("\a  \n \t Делаем уроки ")
+time.sleep(b4)
 print("Работаем на компьютере")
 time.sleep(a4)
 print("\a  \n \t Английский язык")
@@ -22,7 +33,7 @@ print("\a  \n \t Cольфеджио ")
 time.sleep(b4)
 print("\a Работаем на компьютере")
 time.sleep(a4)
-print("\a  \n \t Окружающий мир ")
+print("\a  \n \t Спорт ")
 time.sleep(b4)
 print("\a Работаем на компьютере")
 time.sleep(a4)
