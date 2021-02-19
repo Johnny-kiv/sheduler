a0d863941891aa2d2bfc61b0833230c25d03591a
Johnny <johnny.kiv@yandex.ru>
2021-02-19 16:19:36 +0300

    vv

--- time 4.py
+++ time 4.py
@@ -7,7 +7,8 @@ from tkinter import*
 root = Tk()
 now = datetime.datetime.now()
 c=Canvas(root,bg="yellow",width=800,height=600,cursor="pencil")
+c.create_oval(250,500,550,550,fill="Black")
+c.create_oval(350,500,450,530,fill="gray")
 c.pack()
-
-root.mainloop
+root.mainloop()
 
