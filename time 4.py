dc20d919a41f7559ce5aba25c5bbab5bc98be2b8
Johnny <johnny.kiv@yandex.ru>
2021-02-19 15:52:27 +0300

    vv

--- time 4.py
+++ time 4.py
@@ -2,6 +2,12 @@
 #версия 4
 #Автор: johnny-kiv
 import datetime
+import time
+from tkinter import*
+root = Tk()
 now = datetime.datetime.now()
-print(now)
+c=Canvas(root,bg="yellow",width=800,height=600,cursor="pencil")
+c.pack()
+
+root.mainloop
 
