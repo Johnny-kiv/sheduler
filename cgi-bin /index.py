2f260cb60e5f57814cae7970d6f47846801fa097
Johnny-kiv <johnny.kiv@yandex.ru>
2023-03-10 21:05:08 +0300

    init

--- cgi-bin /index.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python3
-
-print("Content-Type: text/html")    # Хидеры ответа
-print()                             # Пустая строка сразу после хидеров
-# А потом текст ответа
-print("<TITLE>CGI script output</TITLE>")
-print("<H1>This is my first CGI script</H1>")
-print("Hello, world!")
