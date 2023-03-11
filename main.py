18fac3518ff1ff0f4a84af6f11f5e3d36c5e3371
Johnny-kiv <johnny.kiv@yandex.ru>
2023-03-11 12:13:26 +0300

    init

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template
 app = Flask(__name__)
+@app.route("/enter/")
+def enter():
+    return render_template("enter.html")
 @app.route("/")
-def main():
-    return render_template("index.html")
+def print():
+    return render_template("print.html")
 app.run(host="0.0.0.0")
